222f106460829c8c25544fceee464773182e54ef
Allen Zhang <zhangtao25@192.168.2.108>
2025-01-29 21:58:44 +0800

    chore: daily development

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,6 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
 import Report from "./components";
-import { ConfigProvider, theme } from "antd";
-import locale from "antd/locale/de_DE";
-// import { css } from '@emotion/react'
-
+import { ConfigProvider } from "antd";
 function App() {
-  // const [count, setCount] = useState(0)
-  const { token } = theme.useToken();
-  const isDark = token.colorBgBase === "#000";
-
-  const { darkAlgorithm } = theme;
-
   return (
     <>
       <ConfigProvider
@@ -22,12 +8,9 @@ function App() {
           token: {
             colorPrimary: "#0071c2",
           },
-          algorithm: true ? [darkAlgorithm] : [],
         }}
-        locale={locale}
       >
-        {/*{isDark?'是':'否'}*/}
-        <Report locale={"en"} />
+        <Report />
       </ConfigProvider>
     </>
   );
--- src/components/report.tsx
+++ src/components/report.tsx
@@ -1,14 +1,131 @@
-// import { ThemeProvider } from "@emotion/react";
-import { FC } from "react";
-// import { tokenMap } from "./theme.ts";
-import Other from "./widgets/Other.tsx";
-import { CommonProps } from "./types.ts";
+import { ReportProps } from "./types";
+import { FC, useEffect, useMemo, useState } from "react";
+import TopControl from "./widgets/TopControl";
+import { FileCoverageData } from "istanbul-lib-coverage";
+import SummaryHeader from "./widgets/SummaryHeader.tsx";
+import SummaryTreeTable from "./widgets/SummaryTreeTable.tsx";
+import SummaryListTable from "./widgets/SummaryListTable.tsx";
+import FileCoverageDetail from "./widgets/FileCoverageDetail.tsx";
+import { emptyFileCoverageData } from "./helpers/const.ts";
+import { generateCoreDataForEachComponent } from "./helpers/generateCoreDataForEachComponent.ts";
+import { FloatButton } from "antd";
+const onSelectDefault = () => {
+  return Promise.resolve({
+    fileContent: "",
+    fileCoverage: emptyFileCoverageData,
+    fileCodeChange: [],
+  });
+};
 
-const Report: FC<CommonProps> = ({ theme = "light", locale = "en" }) => {
+const Report: FC<ReportProps> = ({
+  name = "untitled", // 报告名称
+  dataSource = {}, // 数据源，概览的map
+  value = "", // 当前选中的文件路径
+  onSelect = onSelectDefault, // 选中文件的回调，返回文件内容和覆盖率，promise
+  defaultOnlyShowChanged = false,
+}) => {
+  // 内部状态
+  const [filenameKeywords, setFilenameKeywords] = useState("");
+  const [showMode, setShowMode] = useState("tree");
+  const [fileCoverage, setFileCoverage] = useState<FileCoverageData>({
+    path: "",
+    statementMap: {},
+    fnMap: {},
+    branchMap: {},
+    s: {},
+    f: {},
+    b: {},
+  });
+  const [fileContent, setFileContent] = useState<string>("");
+  const [fileCodeChange, setFileCodeChange] = useState<number[]>([]);
+  const [onlyChange, setOnlyChange] = useState(Boolean(defaultOnlyShowChanged));
+
+  async function newOnSelect(val: string) {
+    const res = await onSelect(val);
+    setFileContent(res.fileContent);
+    setFileCoverage(res.fileCoverage);
+    setFileCodeChange(res.fileCodeChange);
+    return res;
+  }
+
+  useEffect(() => {
+    newOnSelect(value);
+  }, []);
+
+  const isFile = useMemo(() => {
+    return value.includes(".");
+  }, [value]);
+
+  const mode = useMemo(() => {
+    if (isFile) {
+      return "file";
+    } else {
+      return showMode;
+    }
+  }, [showMode, value]);
+
+  const { treeDataSource, rootDataSource, listDataSource } = useMemo(() => {
+    return generateCoreDataForEachComponent({
+      dataSource,
+      filenameKeywords,
+      value,
+      onlyChange,
+    });
+  }, [dataSource, value, filenameKeywords, onlyChange]);
+
+  function onChangeOnlyChange(v) {
+    setOnlyChange(v);
+  }
+
   return (
-    <>
-      <Other theme={theme} locale={locale}></Other>
-    </>
+    <div>
+      <TopControl
+        onlyChange={onlyChange}
+        filenameKeywords={filenameKeywords}
+        showMode={showMode}
+        onChangeShowMode={(val) => {
+          setShowMode(val);
+        }}
+        onChangeOnlyChange={onChangeOnlyChange}
+        total={listDataSource.length}
+        onChangeKeywords={(val) => {
+          setFilenameKeywords(val);
+        }}
+      />
+      <SummaryHeader
+        reportName={name}
+        data={rootDataSource}
+        value={value}
+        onSelect={newOnSelect}
+      />
+      {mode === "file" &&
+        Object.keys(fileCoverage).length > 0 &&
+        Object.keys(fileContent).length > 0 && (
+          <FileCoverageDetail
+            fileContent={fileContent}
+            fileCodeChange={fileCodeChange}
+            fileCoverage={fileCoverage}
+          />
+        )}
+      <div>
+        <SummaryTreeTable
+          style={{
+            display: mode === "tree" ? "block" : "none",
+          }}
+          dataSource={treeDataSource}
+          onSelect={newOnSelect}
+        />
+        <SummaryListTable
+          style={{
+            display: mode === "list" ? "block" : "none",
+          }}
+          dataSource={listDataSource}
+          onSelect={newOnSelect}
+          filenameKeywords={filenameKeywords}
+        />
+      </div>
+      <FloatButton.BackTop />
+    </div>
   );
 };
 
